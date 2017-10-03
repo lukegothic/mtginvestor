ba71ece889d78405d1183561d99d197aef5c6951
Iván Pérez <lukegothic@gmail.com>
2017-10-03 13:03:22 +0200

    organizacion

--- deckbox_getinventory.py
+++
@@ -1,7 +0,0 @@
-import csv
-import requests
-
-req = requests.get("https://deckbox.org/sets/export/125700?format=csv&f=&s=&o=&columns=Image%20URL")
-
-with open("test.csv", "w") as f:
-    f.write(req.text);
